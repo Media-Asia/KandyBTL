20e9b0e9f735c3b16cb481d2767874b2874c7f9f
Media Asia <talk@mediaasia.lk>
2025-07-14 13:42:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -202,7 +202,7 @@ export default function GlobalWebsite() {
           <video
             className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto object-cover transform -translate-x-1/2 -translate-y-1/2 scale-110"
             /*Hero Section Background Video */
-            src="/0703.mp4"
+            src="/assets/0703.mp4"
             autoPlay
             muted
             loop
